ce1ef950ede8d37f508345258878d1807f8d3f2d
kenny robert <skenrobert@gmail.com>
2023-03-04 15:14:49 -0500

    first commit

---
+++ zprojectapp/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from .models import Project
+
+class ProjectSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Project
+    # fields = '__all__' TODO: below other form
+    fields = ('id','title','description','technology','created_at')
+    read_only_fields = ('created_at',)
